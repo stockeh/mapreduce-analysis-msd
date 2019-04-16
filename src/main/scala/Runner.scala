ddbbe1c3e1b90f7a16fc74c812eba7819d759861
Jason Stock <stock@providence.cs.colostate.edu>
2019-04-15 23:33:20 -0600

    Test and Train percentage

--- src/main/scala/Runner.scala
+++ src/main/scala/Runner.scala
@@ -30,26 +30,34 @@ object Runner {
     // specify layers for the neural network:
     // input layer of size 10 (features), three intermediate of size 9
     // and output of size 86 (classes)
-    val layers = Array[Int](10, 15, 20, 25, 30, 35, 45, 50, 65, 75, 86)
+    val layers = Array[Int](10, 15, 20, 25, 30, 35, 40, 45)
 
     // create the trainer and set its parameters
     val trainer = new MultilayerPerceptronClassifier()
       .setLayers(layers)
       .setBlockSize(128)
       .setSeed(1234L)
-      .setMaxIter(100)
+      .setMaxIter(1000)
 
     // train the model
     val model = trainer.fit(train)
 
     // compute accuracy on the test set
-    val result = model.transform(test)
-    val predictionAndLabels = result.select("prediction", "label")
-    val evaluator = new MulticlassClassificationEvaluator()
+    val testresult = model.transform(test)
+    val testpredictionAndLabels = testresult.select("prediction", "label")
+    val testevaluator = new MulticlassClassificationEvaluator()
       .setMetricName("accuracy")
 
-    println(s"Test set accuracy = ${evaluator.evaluate(predictionAndLabels)}")
+    println(s"Test set accuracy = ${testevaluator.evaluate(testpredictionAndLabels)}")
 
+    val trainresults = model.transform(train)
+    val trainpredictionAndLabels = trainresults.select("prediction", "label")
+    val trainevaluator = new MulticlassClassificationEvaluator()
+      .setMetricName("accuracy")
+
+    println(s"Train set accuracy = ${trainevaluator.evaluate(trainpredictionAndLabels)}")
+
+    
     spark.stop()
   }
 }
